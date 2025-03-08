42f887065a95e257509a25d0c42e727274e564ee
shai37x <shinevmadathilpayyoli@gmail.com>
2025-03-08 17:15:25 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Home from './pages/Home'
 import Navbar from './components/Navbar'
+import Portfolio from './pages/Portfolio'
+import Login from './pages/Login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <BrowserRouter> 
+    <Navbar />   
+    
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        
+      </Routes>
+      
+    </BrowserRouter>
+
   )
 }
-
+export default App 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import React from "react";
+
 const Home = () => {
     return (
       <div className="text-center p-10">
         <h1 className="text-3xl font-bold text-gray-800">Welcome to Greyscale</h1>
-        <p className="text-gray-600">Your best photography portfolio platform.</p>
+        <p style={{color:"red"}}>Your best photography portfolio platform.</p>
       </div>
     );
   };
